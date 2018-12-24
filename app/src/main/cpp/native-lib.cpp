1e0fea2d6821a7f3554c141a846fcd80493db77f
kinsomyjs <FJS0420@outlook.com>
2018-12-24 16:25:04 +0800

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,102 @@
 #include <jni.h>
 #include <string>
+#include <iostream>
+#include <dirent.h>
+#include "LogUtils.h"
+#include <android/bitmap.h>
+
+extern "C" {
+
+using namespace std;
+
+void showAllFiles(string dir_name) {
+
+    if (dir_name.empty()) {
+        LOGE("dir_name is null !");
+        return;
+    }
+
+    //打开文件夹
+    DIR *dir = opendir(dir_name.c_str());
+
+    //检查是否是dir
+    if (NULL == dir) {
+        LOGE("Can't open the dir. Check path if the path is correct ! ")
+        return;
+    }
+
+    struct dirent *file;
+
+    //遍历读取dir下的文件
+    while ((file = readdir(dir)) != NULL) {
+        //跳过 "." 和 ".."
+        if (strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0) {
+            LOGV("ignore . and ..");
+            continue;
+        }
+
+        if (file->d_type == DT_DIR) {
+            //如果是dir类型就递归
+            string filePath = dir_name + "/" + file->d_name;
+            showAllFiles(filePath);
+        } else {
+            //如果是file类型就输出
+            LOGI("filePath: %s/%s", dir_name.c_str(), file->d_name);
+        }
+    }
+    closedir(dir);
+}
+
+
+JNIEXPORT void JNICALL
+Java_com_kinsomy_learnndk_MainActivity_showDir(JNIEnv *env, jobject instance, jstring dirPath_) {
+    const char *dirPath = env->GetStringUTFChars(dirPath_, 0);
+    showAllFiles(string(dirPath));
+    env->ReleaseStringUTFChars(dirPath_, dirPath);
+
+}
+}
+
+extern "C" {
+JNIEXPORT void JNICALL
+Java_com_kinsomy_learnndk_MainActivity_passBitmap(JNIEnv *env, jobject instance, jobject bitmap) {
+    if (NULL == bitmap) {
+        LOGE("bitmap is null !");
+        return;
+    }
+
+    AndroidBitmapInfo info;
+    int result;
+
+    result = AndroidBitmap_getInfo(env, bitmap, &info);
+    if (result != ANDROID_BITMAP_RESULT_SUCCESS) {
+        LOGE("AndroidBitmao_getInfo failed, result: %d", result);
+        return;
+    }
+
+    LOGD("bitmap width: %d, height: %d, format: %d, stride: %d",
+         info.width, info.height, info.format, info.stride);
+
+    //获取像素信息
+    unsigned char *addrPtr;
+    result = AndroidBitmap_lockPixels(env, bitmap, reinterpret_cast<void **> (&addrPtr));
+    if (result != ANDROID_BITMAP_RESULT_SUCCESS) {
+        LOGE("AndroidBitmap_lockPixels failed, result: %d", result);
+        return;
+    }
+
+    int length = info.stride * info.height;
+    for (int i = 0; i < length; ++i) {
+        LOGD("value: %x", addrPtr[i])
+    }
+
+    result = AndroidBitmap_unlockPixels(env, bitmap);
+    if (result != ANDROID_BITMAP_RESULT_SUCCESS) {
+        LOGE("AndroidBitmap_unlockPixels failed, result: %d", result);
+    }
+}
+}
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_kinsomy_learnndk_MainActivity_stringFromJNI(
@@ -14,7 +111,7 @@ JNIEXPORT void JNICALL
 Java_com_kinsomy_learnndk_MainActivity_test(JNIEnv *env, jobject instance) {
 
     jclass clazz = env->GetObjectClass(instance);
-    jmethodID mid =env->GetMethodID(clazz, "callNullPointerException", "()V");
+    jmethodID mid = env->GetMethodID(clazz, "callNullPointerException", "()V");
     env->CallVoidMethod(instance, mid); // will throw a NullPointerException
     jthrowable exc = env->ExceptionOccurred(); // 检测是否发生异常
     if (exc) {
@@ -24,9 +121,9 @@ Java_com_kinsomy_learnndk_MainActivity_test(JNIEnv *env, jobject instance) {
         env->ExceptionClear(); // 清除掉发生的异常
         jclass newExcCls = env->FindClass("java/lang/IllegalArgumentException");
         env->ThrowNew(newExcCls, "throw from JNI"); // 返回一个新的异常到 Java
-    }else {
+    } else {
         jclass npe = env->FindClass("java/lang/NullPointerException");
-        env->ThrowNew(npe,"throw from JNI");
+        env->ThrowNew(npe, "throw from JNI");
     }
 
 }
--- app/src/main/java/com/kinsomy/learnndk/MainActivity.java
+++ app/src/main/java/com/kinsomy/learnndk/MainActivity.java
@@ -1,11 +1,21 @@
 package com.kinsomy.learnndk;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final String TAG = "MainActivity";
 
 	// Used to load the 'native-lib' library on application startup.
 	static {
@@ -16,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		checkPermission();
+//		showDir("/storage/emulated/0/");
 		// Example of a call to a native method
 		TextView tv = (TextView) findViewById(R.id.sample_text);
 		tv.setText(stringFromJNI());
@@ -26,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
 		} catch (Exception e) {
 			Log.e("MainActivity", "onCreate: " + e);
 		}
+
+		Bitmap bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+		bitmap.eraseColor(0xff336699); // AARRGGBB
+		byte[] bytes = new byte[bitmap.getWidth() * bitmap.getHeight() * 4];
+		Buffer dst = ByteBuffer.wrap(bytes);
+		bitmap.copyPixelsToBuffer(dst);
+		// ARGB_8888 真实的存储顺序是 R-G-B-A
+		Log.d(TAG, "R: " + Integer.toHexString(bytes[0] & 0xff));
+		Log.d(TAG, "G: " + Integer.toHexString(bytes[1] & 0xff));
+		Log.d(TAG, "B: " + Integer.toHexString(bytes[2] & 0xff));
+		Log.d(TAG, "A: " + Integer.toHexString(bytes[3] & 0xff));
+		passBitmap(bitmap);
+
 	}
 
 	/**
@@ -38,5 +62,27 @@ public class MainActivity extends AppCompatActivity {
 
 	private void callNullPointerException() throws NullPointerException {
 //		throw new NullPointerException("MainActivity NullPointerException");
+	}
+
+	public native void showDir(String dirPath);
+
+	public native void passBitmap(Bitmap bitmap);
+
+	private void checkPermission() {
+		//检查权限（NEED_PERMISSION）是否被授权 PackageManager.PERMISSION_GRANTED表示同意授权
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+				!= PackageManager.PERMISSION_GRANTED) {
+			//用户已经拒绝过一次，再次弹出权限申请对话框需要给用户一个解释
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+					.WRITE_EXTERNAL_STORAGE)) {
+				Toast.makeText(this, "请开通相关权限，否则无法正常使用本应用！", Toast.LENGTH_SHORT).show();
+			}
+			//申请权限
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+
+		} else {
+			Toast.makeText(this, "授权成功！", Toast.LENGTH_SHORT).show();
+			Log.e("MainActivity", "checkPermission: 已经授权！");
+		}
 	}
 }
